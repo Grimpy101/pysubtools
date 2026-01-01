699278ec48fb233201110506f40f69761f809b29
Gorazd Gorup <gorupgorazd@gmail.com>
2026-01-01 19:22:47 +0100

    Update test for new version of chardet

--- tests/pysubtools_test.py
+++ tests/pysubtools_test.py
@@ -174,7 +174,6 @@ class TestCase(unittest.TestCase):
                       default_flow_style = False)
             parsed.save(result + '.sif', allow_unicode = False)
             continue
-
           assert d == loaded_d
           assert sub == parsed
 
@@ -213,7 +212,8 @@ class TestCase(unittest.TestCase):
     try:
       sub = parser.parse(f)
     except encodings.EncodingError as e:
-      assert e.tried_encodings == []
+      # chardet now detects Thai Industrial Standard encoding!
+      assert e.tried_encodings == ['TIS-620']
 
   def test_subrip_export(self):
     """Tests SubRip exporter on a simple subtitle."""
